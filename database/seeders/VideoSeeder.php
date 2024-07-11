a327fc4595470bcb1853c036851e95154388198f
mehrjoo <70.mehrjoo@gmail.com>
2024-07-11 14:41:04 +0430

    add file videoSeeder and create 15 video fake

---
+++ database/seeders/VideoSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Video;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class VideoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Video::factory()->create([
+            'name' => 'Best Video'
+        ]);
+
+
+        Video::factory()->count(15)->create();
+    }
+}
