4d20f0f8c9acf286775ad3c0a9377a53dc7fc2f4
mehrjoo <70.mehrjoo@gmail.com>
2024-07-13 21:02:03 +0430

    delete file hot in folder public - npm run build

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,15 +1,16 @@
 import './bootstrap';
-import "../css/style.css"
-import "../css/my-style.css"
-import "../css/responsive.css"
-import "../css/bootstrap.min.css"
-import "./jquery-3.2.1.min"
-import "./jquery.sticky-kit.min"
-import "./custom"
-import "./bootstrap.min"
-import "./imagesloaded.pkgd.min"
-import "./grid-blog.min"
-import "./imagesloaded.pkgd.min"
+// import "../css/app.css";
+import "../css/style.css";
+import "../css/my-style.css";
+import "../css/responsive.css";
+import "../css/bootstrap.min.css";
+import "./jquery-3.2.1.min";
+import "./jquery.sticky-kit.min";
+import "./custom";
+import "./bootstrap.min";
+import "./imagesloaded.pkgd.min";
+import "./grid-blog.min";
+import "./imagesloaded.pkgd.min";
 
 import.meta.glob([
     '../img/**',
