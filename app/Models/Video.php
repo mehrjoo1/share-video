ea69f7f95405f6d7c8ce80db460e5141fbb6154b
mehrjoo <70.mehrjoo@gmail.com>
2024-07-19 15:30:12 +0430

    use as Accessor for column length in model video

--- app/Models/Video.php
+++ app/Models/Video.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Video extends Model
 {
     use HasFactory;
+
+    // ساخت یک اکسِسور برای تبدیل عدد به فرمت ساعت
+    // نحوه استفاده بصورت زیر . نام گذاری تابع هم بعد از گت اسم ستون و بعدش کلمه اتریبیوت
+    // این خودش میاد عنصرتون رو اول از همه به فرمت ساعت تغییر میده و موقع نمایش هم نیاز نیست کاری بکنید
+    public function getLengthAttribute($value){
+        return gmdate("H:i:s" , $value);
+    }
 }
