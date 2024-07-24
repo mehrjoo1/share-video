ff79d8cee56cc93990cb862b75680589a266dcd4
mehrjoo <70.mehrjoo@gmail.com>
2024-07-24 21:49:52 +0430

    mass asignment

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Video;
 use Illuminate\Http\Request;
 
 class VideoController extends Controller
@@ -13,6 +14,9 @@ class VideoController extends Controller
     }
 
     public function store(Request $request){
-        dd($request->all());
+        // dd($request->all());
+        Video::create(
+            $request->all()
+        );
     }
 }
